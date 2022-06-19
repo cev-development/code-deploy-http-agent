e5baebc416cddf34e2bc5a2fa31068447c3f929a
CARLOS ANDRE ALVES DE ALMEIDA JUNIOR <60003800@midway.com.br>
2022-06-19 15:59:58 -0300

    testing install script

--- src/services/CommandLineService.js
+++ src/services/CommandLineService.js
@@ -4,6 +4,7 @@ const CommandLineService = () => {
     const execute = async (command) => {
         return new Promise((resolve, reject) => {
             exec(command, (error, stdout, stderr) => {
+                console.log(error, stderr)
                 if (error) reject(error.message)
                 if (stderr) reject(stderr.message)
 
