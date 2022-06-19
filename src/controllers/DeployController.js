2870d169d760cede3276c07932749e97e639a16c
CARLOS ANDRE ALVES DE ALMEIDA JUNIOR <60003800@midway.com.br>
2022-06-19 16:05:38 -0300

    testing install script

--- src/controllers/DeployController.js
+++ src/controllers/DeployController.js
@@ -3,7 +3,11 @@ const commandLineService = CommandLineService();
 
 const DeployController = () => {
     const handle = async (request, response) => {
-        await commandLineService.execute('cd ~/dev/church/church-managment-bff && ./scripts/install.sh');
+        try {
+            await commandLineService.execute('cd ~/dev/church/church-managment-bff && ./scripts/install.sh');
+        } catch (error) {
+            console.log(error)
+        }
 
         return response.status(200).json({ message: "sucesso" });
     }
