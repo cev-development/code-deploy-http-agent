37179fdc0a8b8299b1881a9232a395a096ad56e5
CARLOS ANDRE ALVES DE ALMEIDA JUNIOR <60003800@midway.com.br>
2022-06-19 23:06:27 -0300

    Implementing pm2 settings

--- src/controllers/DeployController.js
+++ src/controllers/DeployController.js
@@ -8,7 +8,7 @@ const DeployController = () => {
             console.log('----- Updating Repository ')
             console.log(await commandLineService.execute('cd ~/dev/church/church-managment-bff/ && git pull'));
             console.log('----- Building ')
-            console.log(await commandLineService.execute("cd ~/dev/church/church-managment-bff/node_modules/ && ls"));
+            console.log(await commandLineService.execute("cd ~/dev/church/church-managment-bff && yarn install --production=false && yarn build"));
             // console.log('----- Building project ')
             // console.log(await commandLineService.execute("cd ~/dev/church/church-managment-bff/ && npx babel-cli src --extensions \".js,.ts\" --out-dir dist --copy-files"));
         } catch (error) {
